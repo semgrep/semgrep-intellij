0094414c438f0ce91a14269e102c5e2bd4ad4155
ajbt200128 <austin@cutedogs.org>
2024-02-02 14:36:48 -0800

    fix login action

--- src/main/kotlin/com/semgrep/idea/actions/LoginAction.kt
+++ src/main/kotlin/com/semgrep/idea/actions/LoginAction.kt
@@ -3,14 +3,13 @@ package com.semgrep.idea.actions
 import com.intellij.ide.BrowserUtil
 import com.intellij.notification.Notification
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.platform.lsp.api.LspServer
 import com.semgrep.idea.lsp.custom_notifications.LoginFinishRequest
 import com.semgrep.idea.lsp.custom_requests.LoginRequest
 
 class LoginAction(private val notification: Notification? = null) : LspAction("Sign In with Semgrep") {
     override fun actionPerformed(e: AnActionEvent, servers: List<com.semgrep.idea.lsp.SemgrepLspServer>) {
         val loginRequest = LoginRequest(servers.first())
-        val response = (servers.first() as LspServer).requestExecutor.sendRequestSync(loginRequest) ?: return
+        val response = loginRequest.sendRequest().join() ?: return
         BrowserUtil.browse(response.url)
         servers.forEach {
             LoginFinishRequest(it, response).sendNotification()
