24bae6f37ac3c108cf34ca3304854b7de2aba80f
Cooper Pierce <cooper@semgrep.com>
2024-12-11 11:58:59 -0500

    feat: legacy LSP messages for old versions
    This should let us have a better transition to newer versions.

--- src/main/kotlin/com/semgrep/idea/lsp/SemgrepLanguageServer.kt
+++ src/main/kotlin/com/semgrep/idea/lsp/SemgrepLanguageServer.kt
@@ -11,17 +11,27 @@ interface SemgrepLanguageServer : LanguageServer {
     @JsonRequest("semgrep/loginStart")
     fun loginStart(params:List<Void> = emptyList()): CompletableFuture<LoginResult>
 
+    @kotlin.Deprecated("Semgrep versions >1.99.0 should use semgrep/loginStart")
+    @JsonRequest("semgrep/login")
+    fun legacyLoginStart(params:List<Void> = emptyList()): CompletableFuture<LoginResult>
+
     @JsonRequest("semgrep/loginStatus")
     fun loginStatus(params:List<Void> = emptyList()): CompletableFuture<LoginStatusResult>
 
     @JsonRequest("semgrep/loginFinish")
     fun loginFinish(params: LoginResult): CompletableFuture<LoginStatusResult>
 
+
+
     // Notifications
     //
     // We need the List<Void> because of a bug in lsp4j
     // when params are not defined, they get set to null (instead of removing the field)
     // and the server throws an exception
+    @kotlin.Deprecated("Semgrep versions >1.99.0 should use the request version")
+    @JsonRequest("semgrep/loginFinish")
+    fun legacyLoginFinish(params: LoginResult)
+
     @JsonNotification("semgrep/logout")
     fun logout(params: List<Void> = emptyList())
 
